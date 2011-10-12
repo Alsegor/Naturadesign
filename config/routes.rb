498391cfd3d52c1044f9068d0ab7cc6c5cb22b5b
Alsegor <alsegor@gmail.com>
2011-10-12 15:04:46 +0400

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Naturadesign::Application.routes.draw do
   resources :posts
 
   get "home/index"
+  
+  resources :posts do
+  	resources :comments
+	end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
