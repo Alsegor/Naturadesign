ca9c3966e471656e54eccf347f815e16360daa56
Alsegor <alsegor@gmail.com>
2011-10-12 12:49:51 +0400

    default natura

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Naturadesign::Application.config.secret_token = '2f54b4c71acfb38b62d226dc03e648c252642bb52f74829dd8c9b16b9a988073bfb828fa70d36ef4267a3698d02e1e6391b4c6c81e3090f2b82e7c75dca7d7d6'
